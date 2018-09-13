38eb3d7113e480fe8cf01be55109587d9f2d9ec4
Brandon Wood <wood149@purdue.edu>
2018-09-12 20:32:47 -0400

    select from classes

--- hello_data.py
+++ hello_data.py
@@ -23,7 +23,14 @@ INSERT INTO PEOPLE(name, color)
 VALUES(?, ?)
 """, PEOPLE)
 
-print('\n'.join(DB.iterdump())) #quick n dirty : do not use
+#print('\n'.join(DB.iterdump())) #quick n dirty : do not use
 
 for person, color in DB.execute("SELECT name, color FROM people"):
     print(person + ", " + color)
+
+person = DB.execute("SELECT name FROM people WHERE color='Blue'").fetchone()
+print(person)
+
+DB.execute("CREATE TABLE IF NOT EXISTS classes (id INTEGER PRIMARY KEY, person INTEGER NOT NULL, class_name TEXT, FOREIGN KEY (person) REFERENCES people(id))")
+
+classes = [("John Purdue", ["SOC 100", "CS 407"])]
